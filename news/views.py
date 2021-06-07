6dd5c35071c9fe9fb7184ffcf1445e1486b5d288
djambulat <jambulatahmadov99@gmail.com>
2021-06-07 23:01:14 +0300

    added sidebar

--- news/views.py
+++ news/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import *
 
 
 def index(request):
     news = News.objects.all()
+    categoris = Category.objects.all()
     context = {
         'news': news,
-        'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categoris': categoris,
     }
     return render(request, 'news/index.html', context)
