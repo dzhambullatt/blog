74325627c78c6251c42fb5e656188c51a62b2f20
djambulat <jambulatahmadov99@gmail.com>
2021-06-08 22:44:05 +0300

    added new url path and new template

--- news/views.py
+++ news/views.py
@@ -13,3 +13,10 @@ def index(request):
         'categoris': categoris,
     }
     return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categoris = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'categoris': categoris, 'category': category})
