8e33767d81af12cb94c0c0d6f06f58aff7a3d01e
djambulat <jambulatahmadov99@gmail.com>
2021-06-09 18:17:22 +0300

    added urls names

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('category/<int:category_id>', get_category),#динамическое подставление url-адреса и подключение функции
+    path('', index, name='home'),
+    path('category/<int:category_id>', get_category, name='category'),#динамическое подставление url-адреса и подключение функции
 ]
