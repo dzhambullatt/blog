a11036fd6cb23fe53b6fabb9df1ed1ae83481985
djambulat <jambulatahmadov99@gmail.com>
2021-06-19 17:53:23 +0300

    create simple tag

---
+++ news/templatetags/news_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+from news.models import Category
+
+register = template.Library()
+
+
+@register.simple_tag()
+def get_categories():
+    return Category.objects.all()
