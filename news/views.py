1ba8574e40f1c5be11394042c33f1fbe0811bc52
djambulat <jambulatahmadov99@gmail.com>
2021-06-19 18:21:29 +0300

    create inclusion_tag

--- news/views.py
+++ news/views.py
@@ -6,17 +6,14 @@ from .models import *
 
 def index(request):
     news = News.objects.all()
-    categoris = Category.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей',
-        'categoris': categoris,
     }
     return render(request, 'news/index.html', context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categoris = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categoris': categoris, 'category': category})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
