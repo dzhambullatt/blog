d5494908e2346605b9420e5c39d20564f1aae5ca
djambulat <jambulatahmadov99@gmail.com>
2021-06-22 22:52:21 +0300

    work in template

--- news/models.py
+++ news/models.py
@@ -11,6 +11,9 @@ class News(models.Model):
     is_published = models.BooleanField('Опубликовано', default=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"news_id": self.pk})
+
     def __str__(self):
         return self.title
 
--- news/views.py
+++ news/views.py
@@ -17,3 +17,8 @@ def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
+
+
+def view_news(request, news_id):
+    news_item = News.objects.get(pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
