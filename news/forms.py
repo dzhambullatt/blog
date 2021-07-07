fb4ce8fd808b831f497ca68785e44aa59dfffcbd
djambulat <jambulatahmadov99@gmail.com>
2021-07-07 22:38:46 +0300

    updated forms

--- news/forms.py
+++ news/forms.py
@@ -4,7 +4,12 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label="Название")
-    content = forms.CharField(label="Контент", required=False)
+    title = forms.CharField(max_length=150, label="Название", widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label="Контент", required=False, widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "rows": 5
+    }))
     is_published = forms.BooleanField(label="Опубликовано", initial=True)
-    category = forms.ModelChoiceField(empty_label="Выбрать", label="Категория", queryset=Category.objects.all())
+    category = forms.ModelChoiceField(empty_label="Выбрать", label="Категория", queryset=Category.objects.all(), widget=forms.Select(attrs={
+        "class": "form-control"
+    }))
