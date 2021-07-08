d4642a20b05eb19dbe5a71baa2d252b1598e9013
djambulat <jambulatahmadov99@gmail.com>
2021-07-09 02:01:55 +0300

    updated views.py

--- news/views.py
+++ news/views.py
@@ -28,6 +28,8 @@ def view_news(request, news_id):
 def add_news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
