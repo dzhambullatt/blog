361dce4f0361d70a7a3916456c19cee98689f562
djambulat <jambulatahmadov99@gmail.com>
2021-07-09 17:48:46 +0300

    updated forms.py2

--- news/forms.py
+++ news/forms.py
@@ -6,4 +6,5 @@ from .models import News
 class NewsForm(forms.ModelForm):
     class Meta:
         model = News
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
