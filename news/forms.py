b9267bc7e8b268cce2f2e4fa57c478fce76c431f
djambulat <jambulatahmadov99@gmail.com>
2021-07-09 17:53:35 +0300

    updated forms.py3

--- news/forms.py
+++ news/forms.py
@@ -8,3 +8,8 @@ class NewsForm(forms.ModelForm):
         model = News
         # fields = '__all__'
         fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
