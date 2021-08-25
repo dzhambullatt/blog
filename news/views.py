83d0ec64b28c105967215514ffa02a2302859968
djambulat <jambulatahmadov99@gmail.com>
2021-08-25 18:21:22 +0300

    added DetailView

--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .forms import NewsForm
 from .models import *
 
@@ -42,15 +42,20 @@ class NewsByCategory(ListView):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
 
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    # template_name = 'news/news_detail.html'
+
 # def get_category(request, category_id):
 #     news = News.objects.filter(category_id=category_id)
 #     category = Category.objects.get(pk=category_id)
 #     return render(request, 'news/category.html', {'news': news, 'category': category})
 
 
-def view_news(request, news_id):
-    news_item = News.objects.get(pk=news_id)
-    return render(request, 'news/view_news.html', {"news_item": news_item})
+# def view_news(request, news_id):
+#     news_item = News.objects.get(pk=news_id)
+#     return render(request, 'news/view_news.html', {"news_item": news_item})
 
 
 def add_news(request):
